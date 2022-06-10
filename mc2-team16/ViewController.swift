7782c2bf6fffa2b489890a35ba2a776ac50d82df
Vincensa Regina Indarto <vincensaregina@gmail.com>
2022-06-10 16:01:12 +0700

    Initial Commit

---
+++ mc2-team16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mc2-team16
+//
+//  Created by Vincensa Regina on 10/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
