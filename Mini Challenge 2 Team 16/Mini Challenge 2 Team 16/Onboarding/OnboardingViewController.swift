27c0ece7eb0426222cb454b150da5bfc0fba2874
Zacky Ilahi Azmi <102889669+zackyazmi@users.noreply.github.com>
2022-06-20 17:29:59 +0700

    penambahan font

--- Mini Challenge 2 Team 16/Mini Challenge 2 Team 16/Onboarding/OnboardingViewController.swift
+++ Mini Challenge 2 Team 16/Mini Challenge 2 Team 16/Onboarding/OnboardingViewController.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    @IBOutlet weak var onboardingCollectionView: UICollectionView!
+    @IBOutlet weak var nextBtnOnboarding: UIButton!
+    @IBOutlet weak var skipBtnOnboarding: UIButton!
+    @IBOutlet weak var pageControlOnboarding: UIPageControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func nextBtnClicked(_ sender: Any) {
     }
-    */
-
+    
+    
+    @IBAction func skipBtnClicked(_ sender: Any) {
+    }
+    
 }
